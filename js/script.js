ccf0a763792266ef4cd5f3b80e57180b5c6e91d6
ericaostini <ericaostini14@gmail.com>
2023-10-18 16:07:01 +0200

    display select in html

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@
 click button "Avvia Gioco" = visualizzo le celle sul playground
 click singola cella sfondo blu e in console visualizzo indice casella
 */
+
+/*
+select per selezionare tipologia di difficoltà del gioco 
+in base alla difficoltà scelta stamperò un numero di diverso di celle
+*/
+
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", function(){
     const numberCell = 100;
     const playground = document.getElementById("playground");
-    console.log(playground);
     // ciclo for per visualizzare 100 celle 
     for (let i = 1; i <= numberCell; i++){
         const squareEl = displayCell();
@@ -19,7 +24,6 @@ function displayCell(){
     // creo div che formerà la cella a cui assegno le rispettive classi
     let singCell = document.createElement("div");
     singCell.classList.add("cell");
-    console.log(singCell);
     singCell.addEventListener("click", function(){
         singCell.classList.add("changeCol")
     });

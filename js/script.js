21043a93988cf7e6dbbe8c9fa64fdb62dbac417b
ericaostini <ericaostini14@gmail.com>
2023-10-18 16:16:39 +0200

    add calc sqrt numberCell to style.height

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ in base alla difficoltà scelta stamperò un numero di diverso di celle
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", function(){
-    const numberCell = 64;
+    const numberCell = 49;
     const playground = document.getElementById("playground");
     // ciclo for per visualizzare 100 celle 
     for (let i = 1; i <= numberCell; i++){
@@ -26,7 +26,8 @@ function displayCell(numberCell){
     // creo div che formerà la cella a cui assegno le rispettive classi
     let singCell = document.createElement("div");
     singCell.classList.add("cell");
-    singCell.style.width = `calc(100% / ${numCellSq})`
+    singCell.style.width = `calc(100% / ${numCellSq})`;
+    singCell.style.height = `calc(100% / ${numCellSq})`;
     singCell.addEventListener("click", function(){
         singCell.classList.add("changeCol")
     });

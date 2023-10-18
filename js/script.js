d3165b94d5922a523e272722f18bc6eb7b788c5b
ericaostini <ericaostini14@gmail.com>
2023-10-18 15:45:07 +0200

    add "click" button function and displayCell function

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* 
+click button "Avvia Gioco" = visualizzo le celle sul playground
+click singola cella sfondo blu e in console visualizzo indice casella
+*/
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+    const playground = document.getElementById("playground");
+    console.log(playground);
+});
+
+function displayCell(){
+    // creo div che formerà la cella a cui assegno le rispettive classi
+    let singCell = document.createElement("div");
+    singCell.classList.add("cell");
+    console.log(singCell);
+}

ebef41c204dc0ac21411edfa965bb43235d3d166
ericaostini <ericaostini14@gmail.com>
2023-10-18 16:15:01 +0200

    Calc numberCell sqrt and apply to style.width

--- js/script.js
+++ js/script.js
@@ -11,19 +11,22 @@ in base alla difficoltà scelta stamperò un numero di diverso di celle
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", function(){
-    const numberCell = 100;
+    const numberCell = 64;
     const playground = document.getElementById("playground");
     // ciclo for per visualizzare 100 celle 
     for (let i = 1; i <= numberCell; i++){
-        const squareEl = displayCell();
+        const squareEl = displayCell(numberCell);
         playground.append(squareEl);
     }
 });
 
-function displayCell(){
+function displayCell(numberCell){
+    const numCellSq = Math.sqrt(numberCell);
+    console.log(numCellSq)
     // creo div che formerà la cella a cui assegno le rispettive classi
     let singCell = document.createElement("div");
     singCell.classList.add("cell");
+    singCell.style.width = `calc(100% / ${numCellSq})`
     singCell.addEventListener("click", function(){
         singCell.classList.add("changeCol")
     });
